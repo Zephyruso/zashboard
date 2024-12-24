cdbe265b2223d41d89cc66aa9dacb1a241df344e
legiz-ru <legiz.ru@gmail.com>
2024-12-24 14:58:01 +0300

    Update index.ts
    add ru

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,11 +3,13 @@ import { language } from '@/store/settings'
 import { createI18n } from 'vue-i18n'
 import en from './en'
 import zh from './zh'
+import ru from './ru'
 
 export const i18n = createI18n({
   locale: language.value,
   messages: {
     [LANG.EN_US]: en,
     [LANG.ZH_CN]: zh,
+    [LANG.RU_RU]: zh,
   },
 })
